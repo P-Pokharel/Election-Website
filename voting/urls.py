543bd5f9143c28f32e88f1d8ff084eac38d5c077
P-Pokharel <pratyush.pokharel77@gmail.com>
2024-05-05 22:41:21 +0545

    Edit login page and add candidate pages

--- voting/urls.py
+++ voting/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('results/', views.results, name='results'),
     path('login/', views.login, name='login'),
+    path('candidate_list/', views.candidate_list, name='candidate-list'),
+    path('candidate_detail/', views.candidate_detail, name='candidate-detail'),
 ]
--- voting/views.py
+++ voting/views.py
@@ -15,3 +15,13 @@ def login(request):
     template_name = 'voting/login.html'
     context = {}
     return render(request, template_name, context)
+
+def candidate_list(request):
+    template_name = 'voting/candidate_list.html'
+    context = {}
+    return render(request, template_name, context)
+
+def candidate_detail(request):
+    template_name = 'voting/candidate_detail.html'
+    context = {}
+    return render(request, template_name, context)
