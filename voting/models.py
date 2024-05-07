095d771a459f4ebdeda06fe305416a346e784a8d
P-Pokharel <pratyush.pokharel77@gmail.com>
2024-05-07 22:28:09 +0545

    Update model and render info in template

--- voting/models.py
+++ voting/models.py
@@ -6,4 +6,6 @@ class Candidate(models.Model):
     name = models.CharField(blank=False, max_length=120)
     current_position = models.CharField(blank=False, max_length=120)
     desired_position = models.CharField(blank=False, max_length=120)
-    date_of_birth = models.DateField()
+    date_of_birth = models.DateField(blank=True)
+    qualification = models.CharField(blank=True, null=True, max_length=100)
+    vote_count = models.IntegerField(default=0)
--- voting/views.py
+++ voting/views.py
@@ -7,9 +7,24 @@ def candidate_list(request):
     template_name = 'voting/candidate_list.html'
 
     csit_candidates = Candidate.objects.filter(desired_position='Head of Department, BSc.CSIT, Kathmandu BernHardt College')
-    print(request.user)
 
     context = {'csit_candidates': csit_candidates}
+
+    return render(request, template_name, context)
+
+def results(request):
+    template_name = 'voting/results.html'
+
+    csit_candidates = Candidate.objects.all()
+
+    context = {'csit_candidates': csit_candidates}
+    
+    return render(request, template_name, context)
+
+def candidate_detail(request):
+    template_name = 'voting/candidate_detail.html'
+
+    context = {}
     return render(request, template_name, context)
 
 def home(request):
@@ -17,18 +32,8 @@ def home(request):
     context = {}
     return render(request, template_name, context)
 
-def results(request):
-    template_name = 'voting/results.html'
-    context = {}
-    return render(request, template_name, context)
-
 def login(request):
     template_name = 'voting/login.html'
-    context = {}
-    return render(request, template_name, context)
-
-def candidate_detail(request):
-    template_name = 'voting/candidate_detail.html'
     context = {}
     return render(request, template_name, context)
 
